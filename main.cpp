9eae1b28cd3d08e5b543e7bccbe8dc10387699bc
ferho256 <ferho@protonmail.com>
2020-04-18 23:28:09 +0900

    template

--- main.cpp
+++ main.cpp
@@ -1,17 +1,28 @@
+#pragma GCC target("avx2")
+#pragma GCC optimization("O3")
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <numeric>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
+void solve(){
+    cout << setprecision(16);
+
+    cout << 0.1234567890123456789 << endl;
+}
+
 int main(){
-    long long N, K;
 
-    cin >> N >> K;
+    cin.tie(0);
+	  ios::sync_with_stdio(false);
+    
+    solve();
 
-    long long res = N % K;
-
-    cout << min(res, K - res) << endl;
     return 0;
 }
